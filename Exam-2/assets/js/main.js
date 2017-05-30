5caed8ab94515902877316c8e58cad6c44fee168
afjalhossan <its4afjal@gmail.com>
2017-05-30 08:41:12 +0600

    Exam-2

---
+++ Exam-2/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".video-play-botton").magnificPopup({
+            type:'video',
+        });
+
+
+
+        
+
+
+    });
+
+    $(".testimonial-list").owlCarousel({
+       items:1,
+        dots:true,
+        autoplay:true,
+    });
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
