7cc25805f5d952887ff2502bffaee1f841b6774e
afjalhossan <its4afjal@gmail.com>
2017-06-11 09:39:38 +0600

    class-47

---
+++ class-47/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".testemonial-carousel").owlCarousel({
+            items:1,
+            loop:true,
+            nav:false,
+            autoplay:false,
+            dots:true,
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
