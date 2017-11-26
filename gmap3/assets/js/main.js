55c99cc5a52da4e8087ccd23293100f064742a89
afjalhossan <its4afjal@gmail.com>
2017-11-26 15:08:13 +0600

    gmap3

--- gmap3/assets/js/main.js
+++ gmap3/assets/js/main.js
@@ -4,23 +4,35 @@
     jQuery(document).ready(function($){
 
 
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
-        
-        $('[data-toggle="tooltip"]').tooltip();
+       var center = [24.3660248,88.5940547];
+$('#map')
+      .gmap3({
+        center: center,
+        zoom: 14,
+        scrollwheel:false
+      })
+       .marker({
+        position: center,
+        icon: 'http://maps.google.com/mapfiles/marker_green.png',
+
+      })
+      .infowindow({
+        position: center,
+        content: "This is Team location",
+        pixelOffset: new google.maps.Size(0,-30) 
+      })
+      .then(function (infowindow) {
+        infowindow.open(this.get(0)); // this.get(0) return the map (see "get" feature)
+      })
+      .rectangle({
+        bounds: [24.37654, 88.60, 24.358548, 88.5864],
+        fillColor: "#15757A",
+        strokeColor : "#FF512F"
+      })
 
 
 
-        
-
-
-    });
-
-
-    jQuery(window).load(function(){
-
-        
-    });
+      }); // End of documnet ready
 
 
 }(jQuery));	
